8b16cadee144c0ce01f760d9d429d09cf6511a56
Jesse Patching <jesse@patching.ca>
2016-02-05 23:14:09 -0700

    Simplify the plugin, only displaying the first .gpx attachment to a post

--- GpxViewer/Listener.php
+++ GpxViewer/Listener.php
@@ -5,23 +5,36 @@ class ABDS_GpxViewer_Listener
 
     static $twig;
 
+    /**
+     * Add the first .gpx file to the post as an embedded Google Map
+     *
+     * @param string $hookName The hook name
+     * @param string $contents The post contents
+     * @param array $hookParams The hook parameters
+     * @param \XenForo_Template_Abstract $template The template
+     */
     public static function template_hook($hookName, &$contents, array $hookParams, XenForo_Template_Abstract $template) {
         if ($hookName == "message_content") {
             $numAttachments = $hookParams['message']['attach_count'];
             if ($numAttachments > 0) {
 
                 // Are there any .gpx attachments to this post?
-                $hasGpxAttachments = false;
-                $attachments = null;
+                $hasGpxAttachment = null;
                 foreach ($hookParams['message']['attachments'] as $attachment) {
                     if (preg_match('/.gpx/i', $attachment['filename'])) {
-                        $attachments[] = $attachment;
+                        $hasGpxAttachment = true;
+                        break;
                     }
                 }
-
-                if (!$attachments) {
+                if (!$hasGpxAttachment) {
                     return;
                 }
+
+                // Load the visitor's information
+                $visitor = \XenForo_Visitor::getInstance();
+
+                // Load the plugin options
+                $options = \XenForo_Application::get('options');
 
                 // Inialize twig
                 if (!$twig) {
@@ -34,57 +47,48 @@ class ABDS_GpxViewer_Listener
                     ));
                 }
 
-                // Load the visitor's information
-                $visitor = \XenForo_Visitor::getInstance();
+                // Get the path to the attachment
+                $model = new \XenForo_Model_Attachment();
+                $filePath = $model->getAttachmentDataFilePath($attachment);
 
-                // Load the plugin options
-                $options = \XenForo_Application::get('options');
+                // Get the gpx xml
+                $gpxString = file_get_contents($filePath);
+                if (!$gpxString) {
+                    if ($config['debug']) {
+                        echo "Attachment " . $attachment['attachment_id'] ." has no file data, or was not found. Skipping.";
+                    }
+                    continue;
+                }
 
-                // Iterate through all of the attachments
                 $htmlPrefix = null;
                 $htmlPostfix = null;
-                foreach ($attachments as $attachment) {
-                    // Get the path to the attachment
-                    $model = new \XenForo_Model_Attachment();
-                    $filePath = $model->getAttachmentDataFilePath($attachment);
+                try {
+                    // Allow SimpleXML to parse Garmin's odd <gpxx:foo> syntax by trimming off the "gpxx:" portion of the tag
+                    $gpxString = str_replace('<gpxx:', '<', $gpxString);
+                    $gpxString = str_replace('</gpxx:', '</', $gpxString);
+                    $xml = new \SimpleXMLElement($gpxString);
 
-                    // Get the gpx xml
-                    $gpxString = file_get_contents($filePath);
-                    if (!$gpxString) {
+                    // Extract the locations the xml
+                    $locations = self::getLocationsFromXml($xml, $visitor);
+                    if (empty($locations)) {
                         if ($config['debug']) {
-                            echo "Attachment " . $attachment['attachment_id'] ." has no file data, or was not found. Skipping.";
+                            echo "Attachment " . $attachment['attachment_id'] ." has a .gpx extension, but doesn't appear to be valid GPX, skipping.";
                         }
                         continue;
                     }
 
-                    try {
-                        // Allow SimpleXML to parse Garmin's odd <gpxx:foo> syntax by trimming off the "gpxx:" portion of the tag
-                        $gpxString = str_replace('<gpxx:', '<', $gpxString);
-                        $gpxString = str_replace('</gpxx:', '</', $gpxString);
-                        $xml = new \SimpleXMLElement($gpxString);
-
-                        // Extract the locations the xml
-                        $locations = self::getLocationsFromXml($xml, $visitor);
-                        if (empty($locations)) {
-                            if ($config['debug']) {
-                                echo "Attachment " . $attachment['attachment_id'] ." has a .gpx extension, but doesn't appear to be valid GPX, skipping.";
-                            }
-                            continue;
-                        }
-
-                        $htmlPrefix .= static::$twig->render('view.twig', array(
-                            'attachment_id' => $attachment['attachment_id'],
-                            'attachment_filename' => $attachment['filename'],
-                            'locations' => $locations,
-                            'initial_time' => $locations[0]['time'],
-                            'button_colour' => $options->abds_gpxviewer_button_colour,
-                            'google_maps_api_key' => $options->abds_gpxviewer_google_maps_api_key,
-                            'marker_colour' => $options->abds_gpxviewer_marker_colour,
-                            'marker_colour_selected' => $options->abds_gpxviewer_marker_colour_selected,
-                        ));
-                    } catch (Exception $e) {
-                        $htmlPostfix .= "<br /><br /><small>(Bad XML in GPX file " . $attachment['filename'] . ", cannot display as map)</small>";
-                    }
+                    $htmlPrefix .= static::$twig->render('view.twig', array(
+                        'attachment_id' => $attachment['attachment_id'],
+                        'attachment_filename' => $attachment['filename'],
+                        'locations' => $locations,
+                        'initial_time' => $locations[0]['time'],
+                        'button_colour' => $options->abds_gpxviewer_button_colour,
+                        'google_maps_api_key' => $options->abds_gpxviewer_google_maps_api_key,
+                        'marker_colour' => $options->abds_gpxviewer_marker_colour,
+                        'marker_colour_selected' => $options->abds_gpxviewer_marker_colour_selected,
+                    ));
+                } catch (Exception $e) {
+                    $htmlPostfix .= "<br /><br /><small>(Bad XML in GPX file " . $attachment['filename'] . ", cannot display as map)</small>";
                 }
 
                 $contents = $htmlPrefix . $contents . $htmlPostfix;
