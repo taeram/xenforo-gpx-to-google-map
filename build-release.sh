26b46725655c12240c0d0676ae1c7241475211ab
Jesse Patching <jesse@patching.ca>
2016-02-05 23:29:17 -0700

    Updated build script

--- build-release.sh
+++ build-release.sh
@@ -12,14 +12,17 @@ git tag -d $RELEASE_VERSION
 git push origin :refs/tags/$RELEASE_VERSION
 
 git rm .gitignore
+git rm --cached *.sh
+
+composer install --optimize-autoloader
 git rm composer.*
 
-composer install
+mkdir -p library/ABDS/
+git mv GpxViewer library/ABDS/
+
 git add vendor
-git commit -am "Tag for version $RELEASE_VERSION"
+git commit -am "Tag $RELEASE_VERSION"
 git tag $RELEASE_VERSION
 git push --tags
 
-git checkout $CURRENT_MASTER_SHA
-git branch -D master
-git checkout -b master
+rm -f "$0" && git checkout $CURRENT_MASTER_SHA && git branch -D master && git checkout -b master
